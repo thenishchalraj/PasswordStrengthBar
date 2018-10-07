f14387b6c71fc951ca9a8e041965c2e2c626a584
MATRIX <shivamedward@gmail.com>
2018-10-07 16:04:10 +0530

    max and min strength added

--- app/src/main/java/com/android/nishchalraj/passwordstrengthbar/MainActivity.java
+++ app/src/main/java/com/android/nishchalraj/passwordstrengthbar/MainActivity.java
@@ -2,18 +2,23 @@ package com.android.nishchalraj.passwordstrengthbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.nishchalraj.passwordstrength.PasswordStrengthBar;
 
 public class MainActivity extends AppCompatActivity {
 
     PasswordStrengthBar passwordStrengthBar;
+    EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        passwordStrengthBar = findViewById(R.id.progressLinearB);
+        passwordStrengthBar = findViewById(R.id.passwordBarCheck);
+        passwordField = findViewById(R.id.passwordFieldCheck);
+
     }
 }
--- passwordstrength/src/main/java/com/android/nishchalraj/passwordstrength/PasswordStrengthBar.java
+++ passwordstrength/src/main/java/com/android/nishchalraj/passwordstrength/PasswordStrengthBar.java
@@ -1,6 +1,7 @@
 package com.android.nishchalraj.passwordstrength;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -10,23 +11,50 @@ import android.widget.ProgressBar;
  * Created by nishchalraj on 06.10.2018.
  */
 
-public class PasswordStrengthBar{
+public class PasswordStrengthBar extends LinearLayout{
 
-    private View view;
-    private LinearLayout plb;
-    private ProgressBar pb1;
-    private ProgressBar pb2;
-    private ProgressBar pb3;
-    private ProgressBar pb4;
+//    protected View view;
+
+    //UI
+    protected LinearLayout plb;
+    protected ProgressBar pb1;
+    protected ProgressBar pb2;
+    protected ProgressBar pb3;
+    protected ProgressBar pb4;
+
+    protected LayoutInflater mInflater;
 
     public PasswordStrengthBar(Context context) {
-        super();
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        view = inflater.inflate(R.layout.password_strength_bar, null);
-        init();
+        super(context);
+        init(context, null);
     }
 
-    private void init(){
+    public PasswordStrengthBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public PasswordStrengthBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init(context, attrs);
+
+    }
+
+//    public PasswordStrengthBar(Context context) {
+//        super();
+//        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        view = inflater.inflate(R.layout.password_strength_bar, null);
+//        init();
+//    }
+
+    protected void init(Context context, AttributeSet attrs){
+        mInflater = LayoutInflater.from(context);
+
+        //load view from xml
+        View view = mInflater.inflate(R.layout.password_strength_bar,this,true);
+
+        //init UI
         plb = view.findViewById(R.id.progressLinearBar);
         pb1 = view.findViewById(R.id.pBar1);
         pb2 = view.findViewById(R.id.pBar2);
@@ -34,11 +62,14 @@ public class PasswordStrengthBar{
         pb4 = view.findViewById(R.id.pBar4);
     }
 
-/*
-setProgress()
+    //to get the maximum and minimum value to which the password strength can be calculated
+    public int getMax(){
+        return 100;
+    }
 
- */
-
+    public int getMin(){
+        return 0;
+    }
 
 
 }
